f2f36cc2c85ddbe2bf65c10ebe32be85bec2125f
Kobe Thuwis <kobe.thuwis@infofarm.be>
2025-02-14 16:25:53 +0100

    fix

--- deployment/upload_to_aws_ecr.sh
+++ deployment/upload_to_aws_ecr.sh
@@ -9,7 +9,7 @@ AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 # Tag the Docker image
-docker tag $ECR_REPOSITORY_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY_NAME:$MY_TAG
+docker tag $ECR_REPOSITORY_NAME:$MY_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY_NAME:$MY_TAG
 
 # Push the Docker image to the ECR repository
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY_NAME:$MY_TAG
