2977851663a3845cd55bad151eee6400fd1d398a
Kobe Thuwis <kobethuwis26@gmail.com>
2026-03-17 08:56:41 +0100

    add health check && venv && error parsing

--- deployment/run_local_docker.sh
+++ deployment/run_local_docker.sh
@@ -2,4 +2,9 @@
 
 MY_TAG=$1
 
+if [ -z "$MY_TAG" ]; then
+    echo "Error: No tag provided. Add an argument when executing the script to set the tag."
+    exit 1
+fi
+
 #TODO: Run the Docker container
--- deployment/run_remote_docker.sh
+++ deployment/run_remote_docker.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+MY_TAG=$1
+
+if [ -z "$MY_TAG" ]; then
+    echo "Error: No tag provided. Add an argument when executing the script to set the tag."
+    exit 1
+fi
+
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 AWS_REGION=$(aws configure get region)
-MY_TAG=$1
 
 #TODO: Run the Docker container
 
